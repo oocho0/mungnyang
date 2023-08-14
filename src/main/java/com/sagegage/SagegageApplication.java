f9d274998f34b1dfed8a505f79dfc94fe9af72d6
Juhee Kim <rlawngml59@gmail.com>
2023-08-14 14:25:11 +0900

    project init

---
+++ src/main/java/com/sagegage/SagegageApplication.java
@@ -0,0 +1,13 @@
+package com.sagegage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagegageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagegageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sagegage/SagegageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagegage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagegageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
