df2392e9832a563a63647291c8b7687af0a7642c
Juhee Kim <rlawngml59@gmail.com>
2023-08-15 22:40:49 +0900

    프로젝트 진행 사항
    - 프로젝트 의존성 추가 및 환경 설정
    - 회원 권한 Role enum 생성
    - 공통 엔티티 DateEntity class 생성
    - 공통 엔티티 WriterEntity class 생성
    - 회원 엔티티 Member class 생성
    - 회원 레포지토리 MemberRepository interface 생성

---
+++ src/main/java/com/mungnyang/constant/Role.java
@@ -0,0 +1,5 @@
+package com.mungnyang.constant;
+
+public enum Role {
+    USER, ADMIN
+}
---
+++ src/main/java/com/mungnyang/entity/Member.java
@@ -0,0 +1,34 @@
+package com.mungnyang.entity;
+
+import com.mungnyang.constant.Role;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "member")
+
+public class Member extends DateEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long memberId;
+    @Column(nullable = false, length = 10)
+    private String name;
+    @Column(nullable = false, length = 30)
+    private String email;
+    @Column(nullable = false, length = 50)
+    private String password;
+    @Lob
+    @Column(nullable = false)
+    private String address;
+    @Column(nullable = false, length = 15)
+    private String tel;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 10)
+    private Role role;
+
+}
