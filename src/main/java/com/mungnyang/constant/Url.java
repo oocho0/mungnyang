68cbcfdde0cdc971ac34bffff1d02be0bab52f15
Juhee Kim <rlawngml59@gmail.com>
2023-09-18 13:16:12 +0900

    프로젝트 진행 사항
    url 바꿈

--- src/main/java/com/mungnyang/constant/Url.java
+++ src/main/java/com/mungnyang/constant/Url.java
@@ -7,6 +7,6 @@ public class Url {
     public static final String LOGIN_FAIL = "/member/login/error";
     public static final String LOGOUT = "/member/logout";
     public static final String KAKAO_LOGIN_REDIRECT_URI = "/member/kakao-token";
-    public static final String FROM_URL = "http://localhost:8080";
+    public static final String FROM_URL = "http://ec2-3-34-84-60.ap-northeast-2.compute.amazonaws.com:8080";
 
 }
